9e1beab9fa9cf678fa572e7fcb84132f339b8e02
keunsulee <i.knsulee@gmail.com>
2016-09-14 19:11:45 +0900

    ....

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.booklisting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void search(View view) {
+        TextView keywordTextView = (TextView) findViewById(R.id.keyword);
+        CharSequence text = keywordTextView.getText();
+
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
