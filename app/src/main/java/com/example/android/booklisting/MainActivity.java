76900df3a3ea19d8336a9f81529725c8e44b9fa6
keunsulee <i.knsulee@gmail.com>
2016-09-15 16:16:04 +0900

    ....

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
+        // When orientation is changed, search again.
         search(null);
     }
 
